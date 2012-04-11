92e803825bc2785b854113b5583e8df931d60a77
Morten Nilsen <morten@runsafe.no>
2012-04-11 20:34:27 +0200

    IntelliJ IDEA!

--- src/no/runsafe/framework/item/RunsafeItem.java
+++ src/no/runsafe/framework/item/RunsafeItem.java
@@ -1,5 +1,18 @@
 package no.runsafe.framework.item;
 
-public class RunsafeItem {
+import org.bukkit.entity.Item;
 
+public class RunsafeItem 
+{
+	public RunsafeItem(Item toWrap)
+	{
+		bukkitItem = toWrap;
+	}
+	
+	public int getEntityId()
+	{
+		return bukkitItem.getEntityId();
+	}
+	
+	private Item bukkitItem;
 }
