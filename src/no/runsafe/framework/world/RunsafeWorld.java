2fbf3b29c153d0704a03ed963bd6f4272a174a3b
Morten Nilsen <morten@runsafe.no>
2012-04-12 20:19:48 +0200

    add method to get raw object

--- src/no/runsafe/framework/world/RunsafeWorld.java
+++ src/no/runsafe/framework/world/RunsafeWorld.java
@@ -2,17 +2,22 @@ package no.runsafe.framework.world;
 
 import org.bukkit.World;
 
-public class RunsafeWorld {
-
+public class RunsafeWorld
+{
 	public RunsafeWorld(World toWrap)
 	{
 		bukkitWorld = toWrap;
 	}
-	
+
 	public String getName()
 	{
 		return bukkitWorld.getName();
 	}
-	
+
+	public World getRaw()
+	{
+		return bukkitWorld;
+	}
+
 	private World bukkitWorld;
 }
