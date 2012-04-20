c3a9713d5ee734c114bd641861b5f2181f19ce5c
Morten Nilsen <morten@runsafe.no>
2012-04-21 00:48:20 +0200

    Improvements to command system

--- src/no/runsafe/framework/command/RunsafeCommandHandler.java
+++ src/no/runsafe/framework/command/RunsafeCommandHandler.java
@@ -27,8 +27,14 @@ public class RunsafeCommandHandler implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
 		if(sender instanceof Player)
+		{
+			if(commandObject.requiredPermission() != null && !sender.hasPermission(commandObject.requiredPermission()))
+			{
+				sender.sendMessage("No access to that command.");
+				return true;
+			}
 			return commandObject.Execute(new RunsafePlayer((Player)sender), args);
-
+		}
 		else
 			return commandObject.Execute(args);
 	}
