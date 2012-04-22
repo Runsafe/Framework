552782855d6a73da25dc33f20136051c7f280e8f
Morten Nilsen <morten@runsafe.no>
2012-04-23 00:49:27 +0200

    Offline support

--- src/no/runsafe/framework/server/player/RunsafePlayer.java
+++ src/no/runsafe/framework/server/player/RunsafePlayer.java
@@ -1,22 +1,56 @@
 package no.runsafe.framework.server.player;
 
+import com.avaje.ebean.enhance.ant.OfflineFileTransform;
+import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.inventory.RunsafeInventory;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 public class RunsafePlayer
 {
+	public RunsafePlayer(String playerName)
+	{
+		Server server = RunsafePlugin.getPluginKernel().getComponent(Server.class);
+		player = server.getPlayer(playerName);
+		if(player == null)
+			basePlayer = server.getOfflinePlayer(playerName);
+	}
+
 	public RunsafePlayer(Player toWrap)
 	{
 		player = toWrap;
+		basePlayer = toWrap;
 	}
 
 	public String getName()
 	{
-		return player.getName();
+		return basePlayer.getName();
+	}
+
+	public boolean hasPlayedBefore()
+	{
+		return basePlayer.hasPlayedBefore();
+	}
+
+	public boolean isOnline()
+	{
+		return basePlayer.isOnline();
+	}
+
+	public boolean isOP()
+	{
+		return basePlayer.isOp();
+	}
+
+	public boolean isWhitelisted()
+	{
+		return basePlayer.isWhitelisted();
 	}
 
 	public float getXP()
@@ -39,10 +73,10 @@ public class RunsafePlayer
 		player.setLevel(level);
 	}
 
-    public void sendBlockChange(RunsafeLocation location, int itemId, byte data)
-    {
-        player.sendBlockChange(location.getRaw(), itemId, data);
-    }
+	public void sendBlockChange(RunsafeLocation location, int itemId, byte data)
+	{
+		player.sendBlockChange(location.getRaw(), itemId, data);
+	}
 
 	public void setFallDistance(float distance)
 	{
@@ -59,27 +93,27 @@ public class RunsafePlayer
 		player.teleport(new Location(world.getRaw(), x, y, z));
 	}
 
-    public RunsafeItemStack getItemInHand()
-    {
-        return new RunsafeItemStack(player.getItemInHand());
-    }
+	public RunsafeItemStack getItemInHand()
+	{
+		return new RunsafeItemStack(player.getItemInHand());
+	}
 
-    public RunsafeLocation getLocation()
-    {
-        return new RunsafeLocation(player.getLocation());
-    }
+	public RunsafeLocation getLocation()
+	{
+		return new RunsafeLocation(player.getLocation());
+	}
 
-    public void sendMessage(String message)
-    {
-        player.sendMessage(message);
-    }
+	public void sendMessage(String message)
+	{
+		player.sendMessage(message);
+	}
 
 	public RunsafeWorld getWorld()
 	{
 		return new RunsafeWorld(player.getWorld());
 	}
 
-	public Player getRaw()
+	public OfflinePlayer getRaw()
 	{
 		return this.player;
 	}
@@ -101,4 +135,5 @@ public class RunsafePlayer
 	}
 
 	private Player player;
+	private OfflinePlayer basePlayer;
 }
