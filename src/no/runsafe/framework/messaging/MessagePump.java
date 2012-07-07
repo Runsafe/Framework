92312f2ae00d9ce4415ad0a9e03397e1f2275c8d
Morten Nilsen <morten@runsafe.no>
2012-07-07 18:24:33 +0200

    Plugin changed name

--- src/no/runsafe/framework/messaging/MessagePump.java
+++ src/no/runsafe/framework/messaging/MessagePump.java
@@ -7,52 +7,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MessagePump implements IMessagePump
-{
-	public static IMessagePump GetPump(RunsafePlugin plugin)
-	{
-		Plugin pump = plugin.getServer().getPluginManager().getPlugin("RunsafeMessageBus");
-		if (pump == null || !(pump instanceof IPumpProvider))
+public class MessagePump implements IMessagePump {
+	public static IMessagePump GetPump(RunsafePlugin plugin) {
+		Plugin pump = RunsafePlugin.Instances.get("RunsafeServices");
+		if(pump == null || !(pump instanceof IPumpProvider))
 			return null;
 
 		return ((IPumpProvider) pump).getInstance();
 	}
 
 	@Override
-	public void RegisterService(IMessageBusService service)
-	{
-		if (!services.containsKey(service.getServiceName()))
+	public void RegisterService(IMessageBusService service) {
+		if(!services.containsKey(service.getServiceName()))
 			services.put(service.getServiceName(), new ArrayList<IMessageBusService>());
 
-		if (!services.get(service.getServiceName()).contains(service))
+		if(!services.get(service.getServiceName()).contains(service))
 			services.get(service.getServiceName()).add(service);
 	}
 
 	@Override
-	public void UnregisterService(IMessageBusService service)
-	{
-		if (!services.containsKey(service.getServiceName()))
+	public void UnregisterService(IMessageBusService service) {
+		if(!services.containsKey(service.getServiceName()))
 			return;
 
-		if (!services.get(service.getServiceName()).contains(service))
+		if(!services.get(service.getServiceName()).contains(service))
 			return;
 
 		services.get(service.getServiceName()).remove(service);
 	}
 
 	@Override
-	public Response HandleMessage(Message message)
-	{
+	public Response HandleMessage(Message message) {
 		Response response = new Response();
 		response.setStatus(MessageBusStatus.MISSING);
 
-		if (services.containsKey((message.getTargetService())))
-		{
-			for (IMessageBusService svc : services.get(message.getTargetService()))
-			{
+		if(services.containsKey((message.getTargetService()))) {
+			for(IMessageBusService svc : services.get(message.getTargetService())) {
 				Response reply = svc.processMessage(message);
-				if (reply != null)
-				{
+				if(reply != null) {
 					response = reply;
 					break;
 				}
@@ -62,17 +54,14 @@ public class MessagePump implements IMessagePump
 	}
 
 	@Override
-	public List<Response> HandleMessageAll(Message message)
-	{
-		if (!services.containsKey(message.getTargetService()))
+	public List<Response> HandleMessageAll(Message message) {
+		if(!services.containsKey(message.getTargetService()))
 			return null;
 
 		ArrayList<Response> responses = new ArrayList<Response>();
-		for (IMessageBusService svc : services.get(message.getTargetService()))
-		{
+		for(IMessageBusService svc : services.get(message.getTargetService())) {
 			Response reply = svc.processMessage(message);
-			if (reply != null)
-			{
+			if(reply != null) {
 				responses.add(reply);
 			}
 		}
