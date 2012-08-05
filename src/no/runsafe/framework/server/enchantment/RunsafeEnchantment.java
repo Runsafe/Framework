2b961164a9964d419296c5e82b72f918892d6710
Morten Nilsen <morten@runsafe.no>
2012-08-05 08:05:13 +0200

    Implement wrapper methods

--- src/no/runsafe/framework/server/enchantment/RunsafeEnchantment.java
+++ src/no/runsafe/framework/server/enchantment/RunsafeEnchantment.java
@@ -1,13 +1,52 @@
 package no.runsafe.framework.server.enchantment;
 
+import no.runsafe.framework.server.ObjectWrapper;
+import no.runsafe.framework.server.item.RunsafeItemStack;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
 
 public class RunsafeEnchantment
 {
-    public RunsafeEnchantment(Enchantment toWrap)
-    {
-        enchantment = toWrap;
-    }
+	public RunsafeEnchantment(Enchantment toWrap)
+	{
+		enchantment = toWrap;
+	}
 
-    private final Enchantment enchantment;
+	public String getName()
+	{
+		return enchantment.getName();
+	}
+
+	public int getMaxLevel()
+	{
+		return enchantment.getMaxLevel();
+	}
+
+	public int getStartLevel()
+	{
+		return enchantment.getStartLevel();
+	}
+
+//	public RunsafeEnchantmentTarget getItemTarget()
+//	{
+//		return ObjectWrapper.convert(enchantment.getItemTarget());
+//	}
+
+	public boolean conflictsWith(RunsafeEnchantment enchantment)
+	{
+		return this.enchantment.conflictsWith(enchantment.getRaw());
+	}
+
+	public boolean canEnchantItem(RunsafeItemStack itemStack)
+	{
+		return enchantment.canEnchantItem(itemStack.getRaw());
+	}
+
+	public Enchantment getRaw()
+	{
+		return enchantment;
+	}
+
+	private final Enchantment enchantment;
 }
