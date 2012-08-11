ff024884f42f8d2a00019a4ff248f4add11e41b0
Morten Nilsen <morten@runsafe.no>
2012-08-11 19:58:18 +0200

    whitespace cleanup

--- src/no/runsafe/framework/server/RunsafeLocation.java
+++ src/no/runsafe/framework/server/RunsafeLocation.java
@@ -9,85 +9,85 @@ public class RunsafeLocation
 		location = toWrap;
 	}
 
-    public RunsafeLocation(RunsafeWorld world, double x, double y, double z)
-    {
-        location = new Location(world.getRaw(), x, y, z);
-    }
+	public RunsafeLocation(RunsafeWorld world, double x, double y, double z)
+	{
+		location = new Location(world.getRaw(), x, y, z);
+	}
 
-    public RunsafeLocation(RunsafeWorld world, double x, double y, double z, float yaw, float pitch)
-    {
-        location = new Location(world.getRaw(), x, y, z, yaw, pitch);
-    }
+	public RunsafeLocation(RunsafeWorld world, double x, double y, double z, float yaw, float pitch)
+	{
+		location = new Location(world.getRaw(), x, y, z, yaw, pitch);
+	}
 
-    public double getX()
-    {
-        return location.getX();
-    }
+	public double getX()
+	{
+		return location.getX();
+	}
 
-    public double getY()
-    {
-        return location.getY();
-    }
+	public double getY()
+	{
+		return location.getY();
+	}
 
-    public double getZ()
-    {
-        return location.getZ();
-    }
+	public double getZ()
+	{
+		return location.getZ();
+	}
 
-    public void setYaw(float yaw)
-    {
-        location.setYaw(yaw);
-    }
+	public void setYaw(float yaw)
+	{
+		location.setYaw(yaw);
+	}
 
-    public void setPitch(float pitch)
-    {
-        location.setPitch(pitch);
-    }
+	public void setPitch(float pitch)
+	{
+		location.setPitch(pitch);
+	}
 
-    public float getYaw()
-    {
-        return location.getYaw();
-    }
+	public float getYaw()
+	{
+		return location.getYaw();
+	}
 
-    public float getPitch()
-    {
-        return location.getPitch();
-    }
+	public float getPitch()
+	{
+		return location.getPitch();
+	}
 
-    public void setX(double x)
-    {
-        this.location.setX(x);
-    }
+	public void setX(double x)
+	{
+		this.location.setX(x);
+	}
 
-    public void setY(double y)
-    {
-        this.location.setY(y);
-    }
+	public void setY(double y)
+	{
+		this.location.setY(y);
+	}
 
-    public void setZ(double z)
-    {
-        this.location.setZ(z);
-    }
+	public void setZ(double z)
+	{
+		this.location.setZ(z);
+	}
 
-    public RunsafeWorld getWorld()
-    {
-        return new RunsafeWorld(location.getWorld());
-    }
+	public RunsafeWorld getWorld()
+	{
+		return new RunsafeWorld(location.getWorld());
+	}
 
-    public int getBlockX()
-    {
-        return location.getBlockX();
-    }
+	public int getBlockX()
+	{
+		return location.getBlockX();
+	}
 
-    public int getBlockY()
-    {
-        return location.getBlockY();
-    }
+	public int getBlockY()
+	{
+		return location.getBlockY();
+	}
 
-    public int getBlockZ()
-    {
-        return location.getBlockZ();
-    }
+	public int getBlockZ()
+	{
+		return location.getBlockZ();
+	}
 
 	public Location getRaw()
 	{
