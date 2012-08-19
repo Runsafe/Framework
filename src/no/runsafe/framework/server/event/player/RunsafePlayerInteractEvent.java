5b5e034799f1072250777758a32e40015b17dc4d
Morten Nilsen <morten@runsafe.no>
2012-08-19 02:47:44 +0200

    Add method to remove the used item Add method to get where the item would get placed if it was a block

--- src/no/runsafe/framework/server/event/player/RunsafePlayerInteractEvent.java
+++ src/no/runsafe/framework/server/event/player/RunsafePlayerInteractEvent.java
@@ -1,45 +1,65 @@
 package no.runsafe.framework.server.event.player;
 
+import no.runsafe.framework.server.ObjectWrapper;
+import no.runsafe.framework.server.RunsafeLocation;
+import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.event.CancellableEvent;
-import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class RunsafePlayerInteractEvent extends RunsafePlayerEvent implements CancellableEvent
 {
-    public RunsafePlayerInteractEvent(PlayerInteractEvent toWrap)
-    {
-        super(toWrap);
-        event = toWrap;
-    }
+	public RunsafePlayerInteractEvent(PlayerInteractEvent toWrap)
+	{
+		super(toWrap);
+		event = toWrap;
+	}
 
-    public Action getAction()
-    {
-        return event.getAction();
-    }
+	public Action getAction()
+	{
+		return event.getAction();
+	}
 
-    public Block getBlock()
-    {
-        return event.getClickedBlock();
-    }
+	public RunsafeBlock getBlock()
+	{
+		return ObjectWrapper.convert(event.getClickedBlock());
+	}
 
-    public ItemStack getItemStack()
-    {
-        return event.getItem();
-    }
+	public ItemStack getItemStack()
+	{
+		return event.getItem();
+	}
 
-    @Override
-    public boolean getCancelled()
-    {
-        return event.isCancelled();
-    }
+	public void removeItemStack()
+	{
+		event.getPlayer().getInventory().remove(event.getItem());
+	}
 
-    @Override
-    public void setCancelled(boolean cancel)
-    {
-        event.setCancelled(cancel);
-    }
+	public RunsafeLocation getTargetBlock()
+	{
+		BlockFace face = event.getBlockFace();
+		RunsafeLocation target = new RunsafeLocation(
+			getBlock().getWorld(),
+			event.getClickedBlock().getX() + face.getModX(),
+			event.getClickedBlock().getY() + face.getModY(),
+			event.getClickedBlock().getZ() + face.getModZ()
+		);
+		return target;
+	}
 
-    private final PlayerInteractEvent event;
+	@Override
+	public boolean getCancelled()
+	{
+		return event.isCancelled();
+	}
+
+	@Override
+	public void setCancelled(boolean cancel)
+	{
+		event.setCancelled(cancel);
+	}
+
+	private final PlayerInteractEvent event;
 }
