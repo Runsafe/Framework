25ace081dee80e50b0a0d57b9c0d696b98afe98a
Morten Nilsen <morten@runsafe.no>
2012-08-23 23:33:51 +0200

    Update craftbukkit to 1.3.1-R2.0

--- src/no/runsafe/framework/event/listener/block/BlockBreak.java
+++ src/no/runsafe/framework/event/listener/block/BlockBreak.java
@@ -1,17 +1,18 @@
 package no.runsafe.framework.event.listener.block;
 
-import no.runsafe.framework.event.block.IBlockBreakEvent;
+import no.runsafe.framework.event.block.IBlockBreak;
 import no.runsafe.framework.event.listener.EventRouter;
-import no.runsafe.framework.server.event.block.RunsafeBlockBreakEvent;
+import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.server.ObjectWrapper;
 import no.runsafe.framework.timer.IScheduler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 
-public class BlockBreak extends EventRouter<IBlockBreakEvent, BlockBreakEvent>
+public class BlockBreak extends EventRouter<IBlockBreak, BlockBreakEvent>
 {
-	public BlockBreak(IScheduler scheduler, IBlockBreakEvent handler)
+	public BlockBreak(IOutput output, IScheduler scheduler, IBlockBreak handler)
 	{
-		super(scheduler, handler);
+		super(output, scheduler, handler);
 	}
 
 	@EventHandler
@@ -21,8 +22,12 @@ public class BlockBreak extends EventRouter<IBlockBreakEvent, BlockBreakEvent>
 		super.AcceptEvent(event);
 	}
 
-	public void OnEvent(BlockBreakEvent event)
+	@Override
+	public boolean OnEvent(BlockBreakEvent event)
 	{
-		handler.OnBlockBreakEvent(new RunsafeBlockBreakEvent(event));
+		return handler.OnBlockBreak(
+			ObjectWrapper.convert(event.getPlayer()),
+			ObjectWrapper.convert(event.getBlock())
+		);
 	}
 }
