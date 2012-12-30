68ddf616a25afc331e498fee02fae37ee2836be9
Morten Nilsen <morten@runsafe.no>
2012-12-30 23:11:10 +0100

    Expand player data hooks

--- src/no/runsafe/framework/hook/IPlayerDataProvider.java
+++ src/no/runsafe/framework/hook/IPlayerDataProvider.java
@@ -1,10 +1,13 @@
 package no.runsafe.framework.hook;
 
 import no.runsafe.framework.server.player.RunsafePlayer;
+import org.joda.time.DateTime;
 
 import java.util.HashMap;
 
 public interface IPlayerDataProvider
 {
 	HashMap<String, String> GetPlayerData(RunsafePlayer player);
+	DateTime GetPlayerLogout(RunsafePlayer player);
+	String GetPlayerBanReason(RunsafePlayer player);
 }
