7ca57b4ae78440b55a148e63206e214685404e50
Kruithne <kruithne@gmail.com>
2013-04-30 18:36:00 +0100

    Expanded RunsafeMaterialData

--- src/no/runsafe/framework/server/material/RunsafeMaterialData.java
+++ src/no/runsafe/framework/server/material/RunsafeMaterialData.java
@@ -1,5 +1,7 @@
 package no.runsafe.framework.server.material;
 
+import no.runsafe.framework.server.ObjectWrapper;
+import no.runsafe.framework.server.item.RunsafeItemStack;
 import org.bukkit.material.MaterialData;
 
 public class RunsafeMaterialData
@@ -10,10 +12,60 @@ public class RunsafeMaterialData
         materialData = toWrap;
     }
 
+	public RunsafeMaterialData(int type)
+	{
+		this.materialData = new MaterialData(type);
+	}
+
+	public RunsafeMaterialData(RunsafeMaterial material)
+	{
+		this.materialData = new MaterialData(material.getRaw());
+	}
+
+	public RunsafeMaterialData(int type, byte data)
+	{
+		this.materialData = new MaterialData(type, data);
+	}
+
+	public RunsafeMaterialData(RunsafeMaterial material, byte data)
+	{
+		this.materialData = new MaterialData(material.getRaw(), data);
+	}
+
     public MaterialData getRaw()
     {
         return materialData;
     }
+
+	public byte getData()
+	{
+		return this.materialData.getData();
+	}
+
+	public void setData(byte data)
+	{
+		this.materialData.setData(data);
+	}
+
+	public RunsafeMaterial getItemType()
+	{
+		return ObjectWrapper.convert(this.materialData.getItemType());
+	}
+
+	public int getItemTypeId()
+	{
+		return this.materialData.getItemTypeId();
+	}
+
+	public RunsafeItemStack toItemStack()
+	{
+		return ObjectWrapper.convert(this.materialData.toItemStack());
+	}
+
+	public RunsafeItemStack toItemStack(int amount)
+	{
+		return ObjectWrapper.convert(this.materialData.toItemStack(amount));
+	}
 
     private final MaterialData materialData;
 }
