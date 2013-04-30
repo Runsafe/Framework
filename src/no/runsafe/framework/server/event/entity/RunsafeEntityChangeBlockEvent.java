8347e811033f1a07717b065af6358cffc54baab9
Kruithne <kruithne@gmail.com>
2013-04-30 06:13:43 +0100

    Made RunsafeEntityChangeBlockEvent cancellable as it should be

--- src/no/runsafe/framework/server/event/entity/RunsafeEntityChangeBlockEvent.java
+++ src/no/runsafe/framework/server/event/entity/RunsafeEntityChangeBlockEvent.java
@@ -2,10 +2,11 @@ package no.runsafe.framework.server.event.entity;
 
 import no.runsafe.framework.server.ObjectWrapper;
 import no.runsafe.framework.server.block.RunsafeBlock;
+import no.runsafe.framework.server.event.CancellableEvent;
 import no.runsafe.framework.server.material.RunsafeMaterial;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 
-public class RunsafeEntityChangeBlockEvent extends RunsafeEntityEvent
+public class RunsafeEntityChangeBlockEvent extends RunsafeEntityEvent implements CancellableEvent
 {
 	public RunsafeEntityChangeBlockEvent(EntityChangeBlockEvent toWrap)
 	{
@@ -26,6 +27,18 @@ public class RunsafeEntityChangeBlockEvent extends RunsafeEntityEvent
 	public byte getData()
 	{
 		return this.event.getData();
+	}
+
+	@Override
+	public boolean getCancelled()
+	{
+		return this.event.isCancelled();
+	}
+
+	@Override
+	public void setCancelled(boolean cancel)
+	{
+		this.event.setCancelled(cancel);
 	}
 
 	private final EntityChangeBlockEvent event;
