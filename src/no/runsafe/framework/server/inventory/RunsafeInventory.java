ea4c9ca6f6a6895f16bb4a24b53722a1d42c9a16
Kruithne <kruithne@gmail.com>
2013-05-01 18:43:23 +0100

    Implemented serialization ability to inventories.

--- src/no/runsafe/framework/server/inventory/RunsafeInventory.java
+++ src/no/runsafe/framework/server/inventory/RunsafeInventory.java
@@ -2,8 +2,13 @@ package no.runsafe.framework.server.inventory;
 
 import com.google.common.collect.Lists;
 import no.runsafe.framework.server.ObjectWrapper;
+import no.runsafe.framework.server.RunsafeServer;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.material.RunsafeMaterial;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -138,6 +143,37 @@ public class RunsafeInventory
 	public void setMaxStackSize(int size)
 	{
 		this.inventory.setMaxStackSize(size);
+	}
+
+	public String serialize()
+	{
+		YamlConfiguration serialize = new YamlConfiguration();
+		ConfigurationSection contents = serialize.createSection("contents");
+
+		int index = 0;
+		for (ItemStack item : inventory.getContents())
+		{
+			contents.set(String.valueOf(index), item);
+			index++;
+		}
+		return serialize.saveToString();
+	}
+
+	public void unserialize(String serialized)
+	{
+		inventory.clear();
+		try
+		{
+			YamlConfiguration unserialize = new YamlConfiguration();
+			unserialize.loadFromString(serialized);
+			ConfigurationSection contents = unserialize.getConfigurationSection("contents");
+			for (String index : contents.getKeys(false))
+				inventory.setItem(Integer.valueOf(index), contents.getItemStack(index));
+		}
+		catch (InvalidConfigurationException e)
+		{
+			RunsafeServer.Instance.getLogger().warning(ExceptionUtils.getFullStackTrace(e));
+		}
 	}
 
 	private final Inventory inventory;
