d960057887f44688f79ca6d3bfd1411c70e4cfbe
Kruithne <kruithne@gmail.com>
2013-05-08 00:11:46 +0100

    RunsafeHopper now implements IInventoryHolder

--- src/no/runsafe/framework/server/block/RunsafeHopper.java
+++ src/no/runsafe/framework/server/block/RunsafeHopper.java
@@ -1,8 +1,11 @@
 package no.runsafe.framework.server.block;
 
+import no.runsafe.framework.server.ObjectWrapper;
+import no.runsafe.framework.server.inventory.IInventoryHolder;
+import no.runsafe.framework.server.inventory.RunsafeInventory;
 import org.bukkit.block.Hopper;
 
-public class RunsafeHopper extends RunsafeBlockState
+public class RunsafeHopper extends RunsafeBlockState implements IInventoryHolder
 {
 	public RunsafeHopper(Hopper toWrap)
 	{
@@ -13,6 +16,12 @@ public class RunsafeHopper extends RunsafeBlockState
 	public Hopper getRaw()
 	{
 		return this.hopper;
+	}
+
+	@Override
+	public RunsafeInventory getInventory()
+	{
+		return ObjectWrapper.convert(this.hopper.getInventory());
 	}
 
 	private Hopper hopper;
