e2eeea07718680bafb2162bd5c6a36b8e51519cf
Kruithne <kruithne@gmail.com>
2013-06-01 22:43:13 +0100

    RunsafeEntityCreatePortalEvent is cancellable

--- src/no/runsafe/framework/server/event/entity/RunsafeEntityCreatePortalEvent.java
+++ src/no/runsafe/framework/server/event/entity/RunsafeEntityCreatePortalEvent.java
@@ -1,13 +1,26 @@
 package no.runsafe.framework.server.event.entity;
 
+import no.runsafe.framework.server.event.CancellableEvent;
 import org.bukkit.event.entity.EntityCreatePortalEvent;
 
-public class RunsafeEntityCreatePortalEvent extends RunsafeEntityEvent
+public class RunsafeEntityCreatePortalEvent extends RunsafeEntityEvent implements CancellableEvent
 {
 	public RunsafeEntityCreatePortalEvent(EntityCreatePortalEvent toWrap)
 	{
 		super(toWrap);
 		this.event = toWrap;
+	}
+
+	@Override
+	public boolean getCancelled()
+	{
+		return this.event.isCancelled();
+	}
+
+	@Override
+	public void setCancelled(boolean cancel)
+	{
+		this.event.setCancelled(cancel);
 	}
 
 	EntityCreatePortalEvent event;
