7ad73a631dcdb207d3e44f933dfcfcaa818c5bdb
Morten Nilsen <morten@runsafe.no>
2013-06-08 03:01:18 +0200

    Make a timed cache feature

---
+++ src/no/runsafe/framework/timer/TimedCache.java
@@ -0,0 +1,39 @@
+package no.runsafe.framework.timer;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TimedCache<Key, Value>
+{
+	public TimedCache(IScheduler scheduler)
+	{
+		this.scheduler = scheduler;
+	}
+
+	public Value Cache(Key key, Value value)
+	{
+		RefreshTimer();
+		return cache.putIfAbsent(key, value);
+	}
+
+	private void RefreshTimer()
+	{
+		if (task > 0)
+			scheduler.cancelTask(task);
+
+		task = scheduler.startAsyncTask(
+			new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					cache.clear();
+				}
+			},
+			300
+		);
+	}
+
+	private final ConcurrentHashMap<Key, Value> cache = new ConcurrentHashMap<Key, Value>();
+	private final IScheduler scheduler;
+	private int task;
+}
