61f5db0029168dece84d38bfb701f566d76648fb
Morten Nilsen <morten@runsafe.no>
2013-06-08 16:21:36 +0200

    Remove unused imports

--- src/no/runsafe/framework/server/block/RunsafeBlockState.java
+++ src/no/runsafe/framework/server/block/RunsafeBlockState.java
@@ -1,11 +1,5 @@
 package no.runsafe.framework.server.block;
 
-import no.runsafe.framework.wrapper.ObjectWrapper;
-import no.runsafe.framework.server.RunsafeLocation;
-import no.runsafe.framework.server.RunsafeWorld;
-import no.runsafe.framework.server.chunk.RunsafeChunk;
-import no.runsafe.framework.server.material.RunsafeMaterial;
-import no.runsafe.framework.server.material.RunsafeMaterialData;
 import no.runsafe.framework.wrapper.block.BukkitBlockState;
 import org.bukkit.block.BlockState;
 
