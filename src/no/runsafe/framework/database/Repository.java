f933129ba3f99b9a736db05dcdd97968fc8cc339
Morten Nilsen <morten@runsafe.no>
2013-06-08 18:07:13 +0200

    Add utility method for dealing with longs in the database

--- src/no/runsafe/framework/database/Repository.java
+++ src/no/runsafe/framework/database/Repository.java
@@ -2,6 +2,7 @@ package no.runsafe.framework.database;
 
 import org.joda.time.DateTime;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 /**
@@ -33,5 +34,29 @@ public abstract class Repository implements ISchemaChanges
 		if (dateTime == null)
 			return null;
 		return new Timestamp(dateTime.getMillis());
+	}
+
+	/**
+	 * Use this to read a long value from the database.
+	 * This will work around certain type bugs in the database driver.
+	 *
+	 * @param value A value fetched from the database
+	 * @return A long value or NULL if the value is unsupported
+	 */
+	protected static Long getLongValue(Object value)
+	{
+		if (value instanceof BigDecimal)
+			return ((BigDecimal) value).longValue();
+
+		if (value instanceof String)
+			return Long.valueOf((String) value);
+
+		if (value instanceof Long)
+			return (Long) value;
+
+		if (value instanceof Integer)
+			return Long.valueOf((Integer) value);
+
+		return null;
 	}
 }
