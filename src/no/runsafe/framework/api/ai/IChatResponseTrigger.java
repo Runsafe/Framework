439060f3214bb5940abeef49ec9f5e3192976850
Morten Nilsen <morten@runsafe.no>
2013-07-12 17:54:35 +0200

    Simplify the creation of custom responders

--- src/no/runsafe/framework/api/ai/IChatResponseTrigger.java
+++ src/no/runsafe/framework/api/ai/IChatResponseTrigger.java
@@ -1,6 +1,10 @@
 package no.runsafe.framework.api.ai;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public interface IChatResponseTrigger
 {
-	public String getResponse(String player, String message);
+	public String getResponse(String player, Matcher message);
+	public Pattern getRule();
 }
