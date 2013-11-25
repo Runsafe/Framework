7b3e16377875bf327fbd24427edd8372300b909d
Morten Nilsen <morten@runsafe.no>
2013-11-25 17:26:33 +0100

    Tweak log format a bit

--- src/no/runsafe/framework/internal/RunsafeLogFormatter.java
+++ src/no/runsafe/framework/internal/RunsafeLogFormatter.java
@@ -1,8 +1,8 @@
 package no.runsafe.framework.internal;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Date;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
@@ -12,22 +12,16 @@ public class RunsafeLogFormatter extends SimpleFormatter
 	@Override
 	public synchronized String format(LogRecord record)
 	{
-		timestamp.setTime(record.getMillis());
-		String source;
-		if (record.getSourceClassName() != null) {
-			source = record.getSourceClassName();
-			if (record.getSourceMethodName() != null) {
-				source += " " + record.getSourceMethodName();
-			}
-		} else {
-			source = record.getLoggerName();
-		}
 		String message = formatMessage(record);
 		return String.format(
-			"[%s] (%s %s) %s: %s\n",
-			timestamp, source, record.getLoggerName(), record.getLevel().getName(), message
+			"[%s] %s: %s\n",
+			timestamp.print(record.getMillis()),
+			record.getLevel().getName(),
+			message
 		);
 	}
 
-	private final Date timestamp = new Date();
+	private final DateTimeFormatter timestamp = new DateTimeFormatterBuilder()
+		.appendHourOfDay(2).appendLiteral(':').appendMinuteOfHour(2)
+		.toFormatter();
 }
