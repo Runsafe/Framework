29d85013f3d307cbb385cb1e9db0fa78f3111b47
Kruithne <kruithne@gmail.com>
2013-12-05 00:10:10 +0000

    Write to the output object if we're setup correctly.

--- src/no/runsafe/framework/minecraft/networking/RunsafeServerConnection.java
+++ src/no/runsafe/framework/minecraft/networking/RunsafeServerConnection.java
@@ -2,13 +2,19 @@ package no.runsafe.framework.minecraft.networking;
 
 import net.minecraft.server.v1_6_R3.DedicatedServerConnection;
 import net.minecraft.server.v1_6_R3.MinecraftServer;
+import no.runsafe.framework.api.IOutput;
 
 import java.net.InetAddress;
 
 public class RunsafeServerConnection extends DedicatedServerConnection
 {
-	public RunsafeServerConnection(MinecraftServer server, InetAddress address, int i)
+	public RunsafeServerConnection(MinecraftServer server, InetAddress address, int i, IOutput output)
 	{
 		super(server, address, i);
+		this.output = output;
+
+		this.output.write("RunsafeServerConnection successfully interjected.");
 	}
+
+	private final IOutput output;
 }
