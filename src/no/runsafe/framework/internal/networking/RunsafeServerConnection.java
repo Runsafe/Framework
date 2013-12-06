8fb82515aaba842b607ecf42018ef551be2f506c
Kruithne <kruithne@gmail.com>
2013-12-06 20:11:08 +0000

    Copy the DedicatedServerConnection thread.

--- src/no/runsafe/framework/internal/networking/RunsafeServerConnection.java
+++ src/no/runsafe/framework/internal/networking/RunsafeServerConnection.java
@@ -1,15 +1,50 @@
 package no.runsafe.framework.internal.networking;
 
-import net.minecraft.server.v1_6_R3.DedicatedServerConnection;
+import net.minecraft.server.v1_6_R3.DedicatedServer;
+import net.minecraft.server.v1_6_R3.DedicatedServerConnectionThread;
 import net.minecraft.server.v1_6_R3.MinecraftServer;
+import net.minecraft.server.v1_6_R3.ServerConnection;
 import no.runsafe.framework.api.IOutput;
 
+import java.io.IOException;
 import java.net.InetAddress;
 
-public class RunsafeServerConnection extends DedicatedServerConnection
+public class RunsafeServerConnection extends ServerConnection
 {
-	public RunsafeServerConnection(MinecraftServer server, InetAddress address, int i, IOutput output)
+	public RunsafeServerConnection(MinecraftServer server, InetAddress address, int i, IOutput output) throws IOException
 	{
-		super(server, address, i);
+		super(server);
+		thread = new DedicatedServerConnectionThread(this, address, i);
+		thread.start();
 	}
+
+	public void a()
+	{
+		super.a();
+		thread.b();
+		thread.interrupt();
+	}
+
+	public void b()
+	{
+		thread.a();
+		super.b();
+	}
+
+	public DedicatedServer c()
+	{
+		return (DedicatedServer) super.d();
+	}
+
+	public void a(InetAddress address)
+	{
+		thread.a(address);
+	}
+
+	public MinecraftServer d()
+	{
+		return c();
+	}
+
+	private final DedicatedServerConnectionThread thread;
 }
