917285f01d610a0d6273c71ba2d52b6fb7697b0f
Kruithne <kruithne@gmail.com>
2013-12-06 00:06:36 +0000

    Remove the server connection, we'll go in at the thread level.

--- src/no/runsafe/framework/internal/networking/RunsafeServerConnectionThread.java
+++ src/no/runsafe/framework/internal/networking/RunsafeServerConnectionThread.java
@@ -2,14 +2,16 @@ package no.runsafe.framework.internal.networking;
 
 import net.minecraft.server.v1_6_R3.DedicatedServerConnectionThread;
 import net.minecraft.server.v1_6_R3.ServerConnection;
+import no.runsafe.framework.api.IOutput;
 
 import java.io.IOException;
 import java.net.InetAddress;
 
 public class RunsafeServerConnectionThread extends DedicatedServerConnectionThread
 {
-	public RunsafeServerConnectionThread(ServerConnection connection, InetAddress address, int i) throws IOException
+	public RunsafeServerConnectionThread(ServerConnection connection, InetAddress address, int i, IOutput output) throws IOException
 	{
 		super(connection, address, i);
+		output.logInformation("Runsafe connection thread initialized.");
 	}
 }
