50cfbad3404e2257ab0b348f825338492cef96ce
Morten Nilsen <morten@runsafe.no>
2013-12-07 20:47:09 +0100

    Players are command executors

--- src/no/runsafe/framework/api/player/IPlayer.java
+++ src/no/runsafe/framework/api/player/IPlayer.java
@@ -1,11 +1,12 @@
 package no.runsafe.framework.api.player;
 
+import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.entity.ILivingEntity;
 import no.runsafe.framework.api.minecraft.IAnimalTamer;
 
 public interface IPlayer extends
 	ILivingEntity, IServerOperator, IAnimalTamer, IPlayerGameplay, IPlayerInventory, IPlayerKinematics,
-	IPlayerPermissions, IPlayerControl, IPlayerVisibility, IPlayerUniverse, IPlayerNotification
+	IPlayerPermissions, IPlayerControl, IPlayerVisibility, IPlayerUniverse, IPlayerNotification, ICommandExecutor
 {
 	String getPrettyName();
 	void setPlayerListName(String playerName);
