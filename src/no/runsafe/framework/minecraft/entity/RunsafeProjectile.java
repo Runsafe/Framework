7c28a1b83c4bc8fe9bcb81420360ca4a58935471
Kruithne <kruithne@gmail.com>
2013-12-09 17:51:19 +0000

    Correct getImpaledBlock() function.

--- src/no/runsafe/framework/minecraft/entity/RunsafeProjectile.java
+++ src/no/runsafe/framework/minecraft/entity/RunsafeProjectile.java
@@ -1,7 +1,6 @@
 package no.runsafe.framework.minecraft.entity;
 
 import net.minecraft.server.v1_6_R3.EntityArrow;
-import net.minecraft.server.v1_6_R3.IProjectile;
 import net.minecraft.server.v1_6_R3.NBTTagCompound;
 import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.block.IBlock;
@@ -9,8 +8,6 @@ import no.runsafe.framework.api.entity.IEntity;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
 import no.runsafe.framework.internal.wrapper.entity.BukkitProjectile;
-import no.runsafe.framework.minecraft.RunsafeWorld;
-import no.runsafe.framework.minecraft.player.RunsafePlayer;
 import org.bukkit.entity.Projectile;
 
 import javax.annotation.Nullable;
@@ -26,12 +23,13 @@ public class RunsafeProjectile extends BukkitProjectile
 	@Nullable
 	public IBlock getImpaledBlock()
 	{
-		IProjectile minecraftProjectile = ObjectUnwrapper.convert(this);
+		Projectile projectileEntity = ObjectUnwrapper.convert(this);
+
 		IWorld world = getWorld();
-		if (!(minecraftProjectile instanceof EntityArrow) || world == null)
+		if (!(projectileEntity instanceof EntityArrow) || world == null)
 			return null;
 
-		EntityArrow arrow = (EntityArrow) minecraftProjectile;
+		EntityArrow arrow = (EntityArrow) projectileEntity;
 		NBTTagCompound tag = new NBTTagCompound();
 		arrow.a(tag);
 		return world.getBlockAt(tag.getShort("xTile"), tag.getShort("yTile"), tag.getShort("zTile"));
