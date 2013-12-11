6e993ea276d9b804d67f269b2ea9947f07e6702c
Kruithne <kruithne@gmail.com>
2013-12-11 01:29:28 +0000

    Corrected sign API.

--- src/no/runsafe/framework/api/block/ISign.java
+++ src/no/runsafe/framework/api/block/ISign.java
@@ -2,4 +2,7 @@ package no.runsafe.framework.api.block;
 
 public interface ISign
 {
+	String[] getLines();
+	String getLine(int i);
+	void setLine(int i, String s);
 }
--- src/no/runsafe/framework/minecraft/block/RunsafeSign.java
+++ src/no/runsafe/framework/minecraft/block/RunsafeSign.java
@@ -1,9 +1,10 @@
 package no.runsafe.framework.minecraft.block;
 
+import no.runsafe.framework.api.block.ISign;
 import no.runsafe.framework.internal.wrapper.block.BukkitSign;
 import org.bukkit.block.Sign;
 
-public class RunsafeSign extends BukkitSign implements no.runsafe.framework.api.block.ISign
+public class RunsafeSign extends BukkitSign implements ISign
 {
 	public RunsafeSign(Sign toWrap)
 	{
