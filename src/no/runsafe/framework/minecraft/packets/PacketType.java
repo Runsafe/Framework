2f367385a89c84dd196b212cdde42619c5a4742a
Kruithne <kruithne@gmail.com>
2013-12-11 01:42:02 +0000

    Implemented PacketUpdateSign.

--- src/no/runsafe/framework/minecraft/packets/PacketType.java
+++ src/no/runsafe/framework/minecraft/packets/PacketType.java
@@ -1,11 +1,13 @@
 package no.runsafe.framework.minecraft.packets;
 
 import net.minecraft.server.v1_6_R3.Packet;
+import net.minecraft.server.v1_6_R3.Packet130UpdateSign;
 import net.minecraft.server.v1_6_R3.Packet63WorldParticles;
 
 public enum PacketType
 {
-	WORLD_PARTICLES(Packet63WorldParticles.class);
+	WORLD_PARTICLES(Packet63WorldParticles.class),
+	UPDATE_SIGN(Packet130UpdateSign.class);
 
 	PacketType(Class<? extends Packet> packetClass)
 	{
---
+++ src/no/runsafe/framework/minecraft/packets/PacketUpdateSign.java
@@ -0,0 +1,26 @@
+package no.runsafe.framework.minecraft.packets;
+
+import no.runsafe.framework.internal.networking.NetworkPacket;
+import no.runsafe.framework.minecraft.RunsafeLocation;
+
+public class PacketUpdateSign extends NetworkPacket
+{
+	public PacketUpdateSign(RunsafeLocation location, String[] lines)
+	{
+		this(location.getBlockX(), location.getBlockY(), location.getBlockZ(), lines);
+	}
+
+	public PacketUpdateSign(int x, int y, int z, String[] lines)
+	{
+		setData("x", x);
+		setData("y", y);
+		setData("z", z);
+		setData("lines", lines);
+	}
+
+	@Override
+	public PacketType getPacketType()
+	{
+		return PacketType.UPDATE_SIGN;
+	}
+}
