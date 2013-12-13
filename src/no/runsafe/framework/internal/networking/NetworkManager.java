7fa51b63ff39c28c101f746cd1babd15b5951cea
Kruithne <kruithne@gmail.com>
2013-12-13 22:38:05 +0000

    Use the global IConsole object for debugging here.

--- src/no/runsafe/framework/internal/networking/NetworkManager.java
+++ src/no/runsafe/framework/internal/networking/NetworkManager.java
@@ -1,11 +1,10 @@
 package no.runsafe.framework.internal.networking;
 
-import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.INetworkEvent;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.InjectionPlugin;
-import no.runsafe.framework.internal.log.Debug;
+import no.runsafe.framework.internal.log.Console;
 import no.runsafe.framework.minecraft.event.networking.RunsafeNetworkEvent;
 import no.runsafe.framework.minecraft.event.networking.RunsafeSendPacketEvent;
 import no.runsafe.framework.minecraft.packets.PacketPlayerInfo;
@@ -21,7 +20,9 @@ public class NetworkManager implements INetworkEvent
 	}
 
 	public NetworkManager()
-	{}
+	{
+		console = Console.Global();
+	}
 
 	public void setTabListName(IPlayer player, String name)
 	{
@@ -45,24 +46,25 @@ public class NetworkManager implements INetworkEvent
 	@Override
 	public void onNetworkEvent(RunsafeNetworkEvent networkEvent)
 	{
-		Debug.Global().debugInfo("Network event detected.");
+		console.logInformation("Network event detected.");
 		if (networkEvent instanceof RunsafeSendPacketEvent)
 		{
-			Debug.Global().debugInfo("Network event is instanceof RunsafeSendPacketEvent");
+			console.logInformation("Network event is instanceof RunsafeSendPacketEvent");
 			RunsafeSendPacketEvent sendEvent = (RunsafeSendPacketEvent) networkEvent;
 			NetworkPacket rawPacket = sendEvent.getPacket();
 
 			if (rawPacket instanceof PacketPlayerInfo)
 			{
-				Debug.Global().debugInfo("rawPacket is instanceof PacketPlayerInfo");
+				console.logInformation("rawPacket is instanceof PacketPlayerInfo");
 				PacketPlayerInfo packet = (PacketPlayerInfo) rawPacket;
 
-				Debug.Global().debugInfo("Tab name: " + getTabListName(sendEvent.getPlayer()));
+				console.logInformation("Tab name: " + getTabListName(sendEvent.getPlayer()));
 				packet.setPlayerName(getTabListName(sendEvent.getPlayer()));
 			}
 		}
 	}
 
+	private final IConsole console;
 	private static final int PLAYERLIST_MAXLENGTH = 16;
 	private final Map<String, String> tabListNames = new HashMap<String, String>(0);
 }
