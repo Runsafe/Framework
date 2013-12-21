4f2ada5cf88cdee2bc801a0b50080bd03414bf04
Morten Nilsen <morten@runsafe.no>
2013-12-21 21:49:12 +0100

    Save locales as we go

--- src/no/runsafe/framework/internal/text/Localization.java
+++ src/no/runsafe/framework/internal/text/Localization.java
@@ -1,16 +1,19 @@
 package no.runsafe.framework.internal.text;
 
 import no.runsafe.framework.api.ILocalizer;
+import no.runsafe.framework.internal.log.Console;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Localization implements ILocalizer
 {
 	public Localization(JavaPlugin plugin, GlobalLocale localizer)
 	{
-		locale = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), localizer.getLocale() + ".yml"));
+		resourceBundle = new File(plugin.getDataFolder(), localizer.getLocale() + ".yml");
+		locale = YamlConfiguration.loadConfiguration(resourceBundle);
 	}
 
 	@SuppressWarnings("InstanceMethodNamingConvention")
@@ -18,10 +21,25 @@ public class Localization implements ILocalizer
 	public String _(String value)
 	{
 		if (locale.getString(value) == null)
+		{
 			locale.set(value, value);
-
+			save();
+		}
 		return locale.getString(value);
 	}
 
+	private void save()
+	{
+		try
+		{
+			locale.save(resourceBundle);
+		}
+		catch (IOException e)
+		{
+			Console.Global().logException(e);
+		}
+	}
+
+	private final File resourceBundle;
 	private final YamlConfiguration locale;
 }
