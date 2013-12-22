66375d401b8382066fbdcf285e4b767a1f3b45cb
Morten Nilsen <morten@runsafe.no>
2013-12-22 23:06:17 +0100

    Debuggery

--- src/no/runsafe/framework/internal/log/Root.java
+++ src/no/runsafe/framework/internal/log/Root.java
@@ -4,8 +4,10 @@ import no.runsafe.framework.minecraft.RunsafeServer;
 import org.picocontainer.Startable;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 // This class will hook into the root logger and send logs to a new file.
@@ -13,30 +15,23 @@ public final class Root extends LoggingBase implements Startable
 {
 	public Root(FileManager handler, RunsafeServer server) throws IOException
 	{
-		super(handler.getLogger("root.log"), handler.getFormat("Root"));
-		serverLogger = server.getLogger();
+		super(server.getLogger(), handler.getFormat("Root"));
+		redirect = handler.getLogger("root.log");
 	}
 
 	@SuppressWarnings("MethodWithMultipleLoops")
 	@Override
 	public void start()
 	{
-		log.log(Level.INFO, "Redirecting root logger..");
-		Logger parent = log.getParent();
-		while (parent.getParent() != null)
-			parent = parent.getParent();
-		// Steal the parent loggers output..
-		hookLogger(parent);
+		redirect.log(Level.INFO, "Redirecting root logger..");
+		Enumeration<String> logs = LogManager.getLogManager().getLoggerNames();
+		while (logs.hasMoreElements())
+			redirect.log(Level.INFO, " Found log " + logs.nextElement());
 		// Steal the server loggers output..
-		hookLogger(serverLogger);
-	}
-
-	private void hookLogger(Logger logger)
-	{
-		for (Handler handler : logger.getHandlers())
-			logger.removeHandler(handler);
 		for (Handler handler : log.getHandlers())
-			logger.addHandler(handler);
+			log.removeHandler(handler);
+		for (Handler handler : redirect.getHandlers())
+			log.addHandler(handler);
 	}
 
 	@Override
@@ -44,5 +39,5 @@ public final class Root extends LoggingBase implements Startable
 	{
 	}
 
-	private final Logger serverLogger;
+	private final Logger redirect;
 }
