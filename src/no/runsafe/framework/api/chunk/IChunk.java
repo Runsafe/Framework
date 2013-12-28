e87ad2f1016c5a24e9008563902eea954e489af7
Morten Nilsen <morten@runsafe.no>
2013-12-28 15:06:21 +0100

    Extrach chunk into an interface

---
+++ src/no/runsafe/framework/api/chunk/IChunk.java
@@ -0,0 +1,29 @@
+package no.runsafe.framework.api.chunk;
+
+import no.runsafe.framework.api.IWorld;
+import no.runsafe.framework.api.block.IBlock;
+import no.runsafe.framework.internal.wrapper.block.BukkitBlockState;
+import no.runsafe.framework.minecraft.entity.RunsafeEntity;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public interface IChunk
+{
+	int getX();
+	int getZ();
+	@Nonnull
+	IWorld getWorld();
+	@Nonnull
+	IBlock getBlock(int i, int i1, int i2);
+	@Nonnull
+	List<RunsafeEntity> getEntities();
+	@Nonnull
+	List<BukkitBlockState> getTileEntities();
+	boolean isUnloaded();
+	boolean load(boolean generate);
+	boolean load();
+	boolean unload(boolean b, boolean b1);
+	boolean unload(boolean b);
+	boolean unload();
+}
--- src/no/runsafe/framework/minecraft/chunk/RunsafeChunk.java
+++ src/no/runsafe/framework/minecraft/chunk/RunsafeChunk.java
@@ -1,9 +1,10 @@
 package no.runsafe.framework.minecraft.chunk;
 
+import no.runsafe.framework.api.chunk.IChunk;
 import no.runsafe.framework.internal.wrapper.chunk.BukkitChunk;
 import org.bukkit.Chunk;
 
-public class RunsafeChunk extends BukkitChunk
+public class RunsafeChunk extends BukkitChunk implements IChunk
 {
 	public RunsafeChunk(Chunk toWrap)
 	{
