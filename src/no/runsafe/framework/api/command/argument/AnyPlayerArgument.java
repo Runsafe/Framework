2da0fa1a7bc1ceb4cd244f00657736ff96684f41
Morten Nilsen <morten@runsafe.no>
2014-01-15 21:16:53 +0100

    Remove unnecessary overrides

--- src/no/runsafe/framework/api/command/argument/AnyPlayerArgument.java
+++ src/no/runsafe/framework/api/command/argument/AnyPlayerArgument.java
@@ -1,15 +1,11 @@
 package no.runsafe.framework.api.command.argument;
 
-import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.ICommandExecutor;
-import no.runsafe.framework.api.player.IPlayer;
-import no.runsafe.framework.internal.InjectionPlugin;
 import no.runsafe.framework.internal.extension.RunsafeServer;
 import no.runsafe.framework.internal.extension.player.RunsafeAmbiguousPlayer;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 
 public class AnyPlayerArgument extends PlayerArgument
@@ -59,12 +55,6 @@ public class AnyPlayerArgument extends PlayerArgument
 		if (matches.size() == 1)
 			return matches.get(0);
 		return isRequired() ? Invalid : value;
-	}
-
-	@Override
-	public IPlayer getValue(IPlayer context, Map<String, String> params)
-	{
-		return InjectionPlugin.getGlobalComponent(IServer.class).getPlayer(params.get(name));
 	}
 
 	public static final String Invalid = "\0INVALID\0";
--- src/no/runsafe/framework/api/command/argument/OnlinePlayerArgument.java
+++ src/no/runsafe/framework/api/command/argument/OnlinePlayerArgument.java
@@ -1,16 +1,13 @@
 package no.runsafe.framework.api.command.argument;
 
-import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.api.player.IPlayerVisibility;
-import no.runsafe.framework.internal.InjectionPlugin;
 import no.runsafe.framework.internal.Player;
 import no.runsafe.framework.internal.extension.player.RunsafeAmbiguousPlayer;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 
 public class OnlinePlayerArgument extends PlayerArgument
@@ -88,12 +85,6 @@ public class OnlinePlayerArgument extends PlayerArgument
 				return matches.get(0);
 		}
 		return isRequired() ? Invalid : value;
-	}
-
-	@Override
-	public IPlayer getValue(IPlayer context, Map<String, String> params)
-	{
-		return InjectionPlugin.getGlobalComponent(IServer.class).getOnlinePlayer(context, params.get(name));
 	}
 
 	public static final String Invalid = "\0INVALID\0";
