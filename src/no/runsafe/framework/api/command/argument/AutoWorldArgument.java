6a2ea6947d60c04bee36e54962ff1b4b63d8b9b6
Morten Nilsen <morten@runsafe.no>
2014-01-15 20:01:01 +0100

    Fully automagic world argument!

---
+++ src/no/runsafe/framework/api/command/argument/AutoWorldArgument.java
@@ -0,0 +1,23 @@
+package no.runsafe.framework.api.command.argument;
+
+import no.runsafe.framework.api.command.ICommandExecutor;
+import no.runsafe.framework.api.player.IPlayerUniverse;
+
+import javax.annotation.Nullable;
+
+public class AutoWorldArgument extends WorldArgument
+{
+	public AutoWorldArgument()
+	{
+		super(true);
+	}
+
+	@Nullable
+	@Override
+	public String expand(ICommandExecutor context, @Nullable String value)
+	{
+		if (value == null && context instanceof IPlayerUniverse)
+			return ((IPlayerUniverse) context).getWorldName();
+		return super.expand(context, value);
+	}
+}
