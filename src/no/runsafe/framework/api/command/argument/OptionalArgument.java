8ef00c055fdb328a8198ce10ca0e06d83fe76652
Morten Nilsen <morten@runsafe.no>
2014-01-18 18:00:37 +0100

    Give optional arguments support for default values

--- src/no/runsafe/framework/api/command/argument/OptionalArgument.java
+++ src/no/runsafe/framework/api/command/argument/OptionalArgument.java
@@ -1,10 +1,20 @@
 package no.runsafe.framework.api.command.argument;
 
-public class OptionalArgument extends CommandArgumentSpecification
+import no.runsafe.framework.api.command.ICommandExecutor;
+
+import javax.annotation.Nullable;
+
+public class OptionalArgument extends CommandArgumentSpecification implements IValueExpander
 {
 	public OptionalArgument(String name)
 	{
 		super(name);
+	}
+
+	public OptionalArgument(String name, String defaultValue)
+	{
+		super(name);
+		this.defaultValue = defaultValue;
 	}
 
 	@Override
@@ -18,4 +28,15 @@ public class OptionalArgument extends CommandArgumentSpecification
 	{
 		return false;
 	}
+
+	@Nullable
+	@Override
+	public String expand(ICommandExecutor context, @Nullable String value)
+	{
+		if (value != null)
+			return value;
+		return defaultValue;
+	}
+
+	private String defaultValue = null;
 }
