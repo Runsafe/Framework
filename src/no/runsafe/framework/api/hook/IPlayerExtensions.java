f1da3d0fcaab95bbb729bfc770fc6e3c695225dd
Kruithne <kruithne@gmail.com>
2014-01-28 22:23:27 +0000

    Moved the add/remove permission methods to IPlayerPermissions interface.

--- src/no/runsafe/framework/api/hook/IPlayerExtensions.java
+++ src/no/runsafe/framework/api/hook/IPlayerExtensions.java
@@ -30,8 +30,4 @@ public interface IPlayerExtensions
 	boolean hasPermission(RunsafeFakePlayer player, ImmutableList<String> memberOf, String permission);
 	List<String> getGroups();
 	List<String> find(String playerName);
-	void addPermission(IPlayer player, String permission, String world);
-	void addPermission(IPlayer player, String permission);
-	void removePermission(IPlayer player, String permission, String world);
-	void removePermission(IPlayer player, String permission);
 }
