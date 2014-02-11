a896c8315e8bb97ed31d0ff6bf1adbcb7a9ae0bc
Kruithne <kruithne@gmail.com>
2014-02-11 18:27:18 +0000

    Implemented custom recipe interface.

---
+++ src/no/runsafe/framework/api/item/ICustomRecipe.java
@@ -0,0 +1,12 @@
+package no.runsafe.framework.api.item;
+
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+import java.util.Map;
+
+public interface ICustomRecipe
+{
+	RunsafeMeta getResult();
+	Map<Integer, RunsafeMeta> getRecipe();
+	void addIngredient(int slotID, RunsafeMeta item);
+}
