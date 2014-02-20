32e813a35d0f162e5a9ac1e88a4e02cfe533c9c9
Morten Nilsen <morten@runsafe.no>
2014-02-20 23:28:20 +0100

    Add an enchant argument.

---
+++ src/no/runsafe/framework/api/command/argument/EnchantArgument.java
@@ -0,0 +1,66 @@
+package no.runsafe.framework.api.command.argument;
+
+import no.runsafe.framework.api.command.ICommandExecutor;
+import no.runsafe.framework.api.minecraft.IEnchant;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Enchant;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnchantArgument extends CommandArgumentSpecification implements ITabComplete, IValueExpander
+{
+	public EnchantArgument(boolean required)
+	{
+		this("enchant", required);
+	}
+
+	public EnchantArgument(String name, boolean required)
+	{
+		super(name);
+		this.required = required;
+	}
+
+	@Override
+	public List<String> getAlternatives(IPlayer executor, String partial)
+	{
+		String filter = partial.toLowerCase();
+		List<String> alternates = new ArrayList<String>(Enchant.All.size());
+		for (IEnchant enchant : Enchant.All)
+		{
+			String name = enchant.getName().toLowerCase();
+			if (name.equals(filter) || name.startsWith(filter))
+				alternates.add(name);
+		}
+		return alternates;
+	}
+
+	@Nullable
+	@Override
+	public String expand(ICommandExecutor context, @Nullable String value)
+	{
+		String filter = value.toLowerCase();
+		for (IEnchant enchant : Enchant.All)
+		{
+			String name = enchant.getName().toLowerCase();
+			if (name.equals(filter) || name.startsWith(filter))
+				return name;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isRequired()
+	{
+		return required;
+	}
+
+	@Override
+	public boolean isWhitespaceInclusive()
+	{
+		return false;
+	}
+
+	private final boolean required;
+}
