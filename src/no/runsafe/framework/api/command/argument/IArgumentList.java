f84a5864c0934f6c4bc3d360411e58db5e6128c4
Morten Nilsen <morten@runsafe.no>
2014-02-27 22:13:42 +0100

    Actually deprecate in API.

--- src/no/runsafe/framework/api/command/argument/IArgumentList.java
+++ src/no/runsafe/framework/api/command/argument/IArgumentList.java
@@ -11,18 +11,24 @@ import java.util.Map;
 
 public interface IArgumentList extends Map<String, String>
 {
+	@Deprecated
 	@Nullable
 	IWorld getWorld(String param);
+	@Deprecated
 	@Nullable
 	IPlayer getPlayer(String param);
+	@Deprecated
 	@Nonnull
 	List<IPlayer> getPlayers(String param);
+	@Deprecated
 	@Nullable
 	Enum<?> getEnum(String param);
+	@Deprecated
 	@Nullable
 	<T> T getMappedValue(String param);
 	@Nullable
 	<T> T getValue(String param);
+	@Deprecated
 	@Nonnull
 	<T> Collection<T> getList(String param);
 	boolean isAborted();
