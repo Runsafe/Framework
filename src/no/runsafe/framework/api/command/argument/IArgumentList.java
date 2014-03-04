21ed4825efa00c70c835c08d85ba61cb6eed2924
Morten Nilsen <morten@runsafe.no>
2014-03-04 19:56:46 +0100

    Disconnect IArgumentList from Map and deprecate most the methods that once came from Map. Also, remove deprecated player argument subtypes

--- src/no/runsafe/framework/api/command/argument/IArgumentList.java
+++ src/no/runsafe/framework/api/command/argument/IArgumentList.java
@@ -1,14 +1,47 @@
 package no.runsafe.framework.api.command.argument;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
-public interface IArgumentList extends Map<String, String>
+public interface IArgumentList
 {
 	@Deprecated
-	@Override
 	boolean containsKey(Object key);
 
+	@Deprecated
+	int size();
+
+	@Deprecated
+	boolean isEmpty();
+
+	@Deprecated
+	boolean containsValue(Object value);
+
+	@Deprecated
+	String put(String key, String value);
+
+	@Deprecated
+	String remove(Object key);
+
+	@Deprecated
+	void putAll(Map<? extends String, ? extends String> m);
+
+	@Deprecated
+	void clear();
+
+	@Deprecated
+	Set<String> keySet();
+
+	@Deprecated
+	Collection<String> values();
+
+	@Deprecated
+	Set<Map.Entry<String, String>> entrySet();
+
+	@Nullable
+	String get(Object key);
 	@Nullable
 	<T> T getValue(String param);
 	boolean isAborted();
