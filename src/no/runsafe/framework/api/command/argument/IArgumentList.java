f89c303e0eb1f1d5c85d408a851c535df0025934
Morten Nilsen <morten@runsafe.no>
2014-03-04 20:06:44 +0100

    Remove disused API

--- src/no/runsafe/framework/api/command/argument/IArgumentList.java
+++ src/no/runsafe/framework/api/command/argument/IArgumentList.java
@@ -1,48 +1,18 @@
 package no.runsafe.framework.api.command.argument;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 public interface IArgumentList
 {
-	@Deprecated
-	boolean containsKey(Object key);
-
-	@Deprecated
+	boolean isAborted();
 	int size();
-
-	@Deprecated
-	boolean isEmpty();
-
-	@Deprecated
-	boolean containsValue(Object value);
-
-	@Deprecated
-	String put(String key, String value);
-
-	@Deprecated
-	String remove(Object key);
-
-	@Deprecated
-	void putAll(Map<? extends String, ? extends String> m);
-
-	@Deprecated
-	void clear();
-
-	@Deprecated
-	Set<String> keySet();
-
-	@Deprecated
-	Collection<String> values();
-
-	@Deprecated
-	Set<Map.Entry<String, String>> entrySet();
-
 	@Nullable
 	String get(Object key);
 	@Nullable
 	<T> T getValue(String param);
-	boolean isAborted();
+	@Nonnull
+	Set<Map.Entry<String, String>> entrySet();
 }
--- src/no/runsafe/framework/internal/command/argument/ArgumentList.java
+++ src/no/runsafe/framework/internal/command/argument/ArgumentList.java
@@ -9,7 +9,6 @@ import no.runsafe.framework.api.player.IPlayer;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
@@ -31,69 +30,13 @@ public class ArgumentList implements IArgumentList
 	}
 
 	@Override
-	public boolean isEmpty()
-	{
-		return parameterList.isEmpty();
-	}
-
-	@Override
-	public boolean containsKey(Object key)
-	{
-		return parameterList.containsKey(key);
-	}
-
-	@Override
-	public boolean containsValue(Object value)
-	{
-		return parameterList.containsValue(value);
-	}
-
-	@Override
 	public String get(Object key)
 	{
 		return parameterList.get(key);
 	}
 
-	@Nullable
 	@Override
-	public String put(String key, String value)
-	{
-		return null;
-	}
-
-	@Nullable
-	@Override
-	public String remove(Object key)
-	{
-		return null;
-	}
-
-	@Override
-	public void putAll(@Nonnull Map<? extends String, ? extends String> m)
-	{
-	}
-
-	@Override
-	public void clear()
-	{
-	}
-
 	@Nonnull
-	@Override
-	public Set<String> keySet()
-	{
-		return parameterList.keySet();
-	}
-
-	@Nonnull
-	@Override
-	public Collection<String> values()
-	{
-		return parameterList.values();
-	}
-
-	@Nonnull
-	@Override
 	public Set<Map.Entry<String, String>> entrySet()
 	{
 		return parameterList.entrySet();
@@ -102,7 +45,7 @@ public class ArgumentList implements IArgumentList
 	@Override
 	public boolean isAborted()
 	{
-		if (containsValue(null))
+		if (parameterList.containsValue(null))
 			return true;
 
 		for (IArgument argument : arguments.values())
