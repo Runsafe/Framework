38461cb2e1aae9364cbc85eb7dcf08173d7cb6ec
Kruithne <kruithne@gmail.com>
2014-06-25 01:04:24 +0100

    Corrected Acaia and DarkOak log IDS

--- src/no/runsafe/framework/minecraft/Item.java
+++ src/no/runsafe/framework/minecraft/Item.java
@@ -205,8 +205,8 @@ public class Item implements IEnchantable
 			public static final Item Spruce = new Item(Material.LOG, true, (byte) 1);
 			public static final Item Birch = new Item(Material.LOG, true, (byte) 2);
 			public static final Item Jungle = new Item(Material.LOG, true, (byte) 3);
-			public static final Item Acacia = new Item(Material.LOG, true, (byte) 4);
-			public static final Item DarkOak = new Item(Material.LOG, true, (byte) 5);
+			public static final Item Acacia = new Item(Material.LOG_2, true, (byte) 0);
+			public static final Item DarkOak = new Item(Material.LOG_2, true, (byte) 1);
 
 			private Wood()
 			{
