67fd90f18f5d94c520899daba0fa089105da9fd3
njs41 <njs42@hotmail.com>
2017-05-03 11:11:07 -0400

    Add API for Armour stands

--- src/no/runsafe/framework/minecraft/entity/RunsafeArmourStand.java
+++ src/no/runsafe/framework/minecraft/entity/RunsafeArmourStand.java
@@ -1,9 +1,10 @@
 package no.runsafe.framework.minecraft.entity;
 
+import no.runsafe.framework.api.entity.IArmourStand;
 import no.runsafe.framework.internal.wrapper.entity.BukkitArmourStand;
 import org.bukkit.entity.ArmorStand;
 
-public class RunsafeArmourStand extends BukkitArmourStand
+public class RunsafeArmourStand extends BukkitArmourStand implements IArmourStand
 {
 	public RunsafeArmourStand(ArmorStand toWrap)
 	{
