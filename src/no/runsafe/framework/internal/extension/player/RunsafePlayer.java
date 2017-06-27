9fc470c0c138c5f2be5570a1dda08a4e774659cf
njs41 <njs42@hotmail.com>
2017-06-27 11:22:54 -0400

    Make whois command display a player's unique Id.

--- src/no/runsafe/framework/internal/extension/player/RunsafePlayer.java
+++ src/no/runsafe/framework/internal/extension/player/RunsafePlayer.java
@@ -126,6 +126,7 @@ public class RunsafePlayer extends BukkitPlayer implements IPlayer
 			Location location = player.getLocation();
 			data.put("game.location", String.format("[%.2f,%.2f,%.2f@%s]", location.getX(), location.getY(), location.getZ(), location.getWorld().getName()));
 		}
+		data.put("game.uniqueId", getUniqueId().toString());
 		data.put("game.experience", String.format("%.1f", getXP()));
 		data.put("game.level", String.format("%d", getLevel()));
 		data.put("game.op", isOP() ? "true" : "false");
