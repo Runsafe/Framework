e0a6639faeaad38974bf4c8361bd6511d3f20475
njs41 <njs42@hotmail.com>
2017-07-13 16:05:00 -0400

    Add new IEntityPortal event.

--- src/no/runsafe/framework/internal/event/listener/entity/Portal.java
+++ src/no/runsafe/framework/internal/event/listener/entity/Portal.java
@@ -1,20 +1,19 @@
 package no.runsafe.framework.internal.event.listener.entity;
 
 import no.runsafe.framework.api.EventRouterFactory;
+import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.event.IRunsafeEvent;
-import no.runsafe.framework.api.event.entity.IEntityPortal;
-import no.runsafe.framework.api.log.IConsole;
+import no.runsafe.framework.api.event.entity.IEntityPortalEvent;
 import no.runsafe.framework.internal.event.listener.EventRouterBase;
-import no.runsafe.framework.internal.wrapper.ObjectWrapper;
 import no.runsafe.framework.minecraft.event.entity.RunsafeEntityPortalEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPortalEvent;
 
-public class PortalEvent extends EventRouterBase<IEntityPortal, EntityPortalEvent>
+public class Portal extends EventRouterBase<IEntityPortalEvent, EntityPortalEvent>
 {
-	PortalEvent(IConsole output, IScheduler scheduler, IEntityPortal handler)
+	Portal(IConsole output, IScheduler scheduler, IEntityPortalEvent handler)
 	{
 		super(output, scheduler, handler);
 	}
@@ -29,11 +28,8 @@ public class PortalEvent extends EventRouterBase<IEntityPortal, EntityPortalEven
 	@Override
 	public boolean onEvent(EntityPortalEvent event)
 	{
-		return !handler.OnEntityPortal(
-			ObjectWrapper.convert(event.getEntity()),
-			ObjectWrapper.convert(event.getFrom()),
-			ObjectWrapper.convert(event.getTo())
-		);
+		handler.OnEntityPortalEvent(new RunsafeEntityPortalEvent(event));
+		return false;
 	}
 
 	public static EventRouterFactory Factory()
@@ -43,13 +39,13 @@ public class PortalEvent extends EventRouterBase<IEntityPortal, EntityPortalEven
 			@Override
 			public Class<? extends IRunsafeEvent> getInterface()
 			{
-				return IEntityPortal.class;
+				return IEntityPortalEvent.class;
 			}
 
 			@Override
 			public Listener getListener(IConsole output, IScheduler scheduler, IRunsafeEvent subscriber)
 			{
-				return new PortalEvent(output, scheduler, (IEntityPortal) subscriber);
+				return new Portal(output, scheduler, (IEntityPortalEvent) subscriber);
 			}
 		};
 	}
