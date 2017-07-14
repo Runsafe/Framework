1e19734d9c850775adf70d958c2267d3447796a8
njs41 <njs42@hotmail.com>
2017-07-14 12:19:53 -0400

    Add new IEntityTeleport event.

--- src/no/runsafe/framework/internal/event/listener/entity/Teleport.java
+++ src/no/runsafe/framework/internal/event/listener/entity/Teleport.java
@@ -3,18 +3,17 @@ package no.runsafe.framework.internal.event.listener.entity;
 import no.runsafe.framework.api.EventRouterFactory;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.event.IRunsafeEvent;
-import no.runsafe.framework.api.event.entity.IEntityTeleport;
+import no.runsafe.framework.api.event.entity.IEntityTeleportEvent;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.internal.event.listener.EventRouterBase;
-import no.runsafe.framework.internal.wrapper.ObjectWrapper;
 import no.runsafe.framework.minecraft.event.entity.RunsafeEntityTeleportEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTeleportEvent;
 
-public class TeleportEvent extends EventRouterBase<IEntityTeleport, EntityTeleportEvent>
+public class Teleport extends EventRouterBase<IEntityTeleportEvent, EntityTeleportEvent>
 {
-	TeleportEvent(IConsole output, IScheduler scheduler, IEntityTeleport handler)
+	Teleport(IConsole output, IScheduler scheduler, IEntityTeleportEvent handler)
 	{
 		super(output, scheduler, handler);
 	}
@@ -29,11 +28,8 @@ public class TeleportEvent extends EventRouterBase<IEntityTeleport, EntityTelepo
 	@Override
 	public boolean onEvent(EntityTeleportEvent event)
 	{
-		return !handler.OnEntityTeleport(
-			ObjectWrapper.convert(event.getEntity()),
-			ObjectWrapper.convert(event.getFrom()),
-			ObjectWrapper.convert(event.getTo())
-		);
+		handler.OnEntityTeleport(new RunsafeEntityTeleportEvent(event));
+		return false;
 	}
 
 	public static EventRouterFactory Factory()
@@ -43,13 +39,13 @@ public class TeleportEvent extends EventRouterBase<IEntityTeleport, EntityTelepo
 			@Override
 			public Class<? extends IRunsafeEvent> getInterface()
 			{
-				return IEntityTeleport.class;
+				return IEntityTeleportEvent.class;
 			}
 
 			@Override
 			public Listener getListener(IConsole output, IScheduler scheduler, IRunsafeEvent subscriber)
 			{
-				return new TeleportEvent(output, scheduler, (IEntityTeleport) subscriber);
+				return new Teleport(output, scheduler, (IEntityTeleportEvent) subscriber);
 			}
 		};
 	}
