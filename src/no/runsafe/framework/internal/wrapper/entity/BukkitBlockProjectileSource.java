12832b8f633edef04778e10bc5267f2a1c6ca461
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-09-11 22:24:37 -0400

    Fix issue with block projectile sources.
    Attempt to fix issue where when a player would be shot by a projectile from a dispenser the console would spit out exceptions.

--- src/no/runsafe/framework/internal/wrapper/entity/BukkitBlockProjectileSource.java
+++ src/no/runsafe/framework/internal/wrapper/entity/BukkitBlockProjectileSource.java
@@ -1,33 +1,26 @@
 package no.runsafe.framework.internal.wrapper.entity;
 
 import no.runsafe.framework.api.entity.IBlockProjectileSource;
-import no.runsafe.framework.api.entity.IProjectileSource;
 import no.runsafe.framework.api.block.IBlock;
-import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.internal.wrapper.ObjectWrapper;
+import no.runsafe.framework.minecraft.entity.RunsafeBlockProjectileSource;
 import no.runsafe.framework.minecraft.entity.RunsafeProjectile;
 import no.runsafe.framework.internal.extension.block.RunsafeDispenser;
-import org.apache.commons.lang.Validate;
-import org.bukkit.block.Dispenser;
+import org.bukkit.projectiles.BlockProjectileSource;
 import org.bukkit.util.Vector;
-import org.bukkit.Material;
 
 public abstract class BukkitBlockProjectileSource implements IBlockProjectileSource
 {
-    private final RunsafeDispenser dispenserBlock;
+    private final RunsafeBlockProjectileSource blockProjectileSource;
 
-    public BukkitBlockProjectileSource(Dispenser toWrap)
+    public BukkitBlockProjectileSource(BlockProjectileSource toWrap)
     {
-        dispenserBlock = ObjectWrapper.convert(toWrap);
+        blockProjectileSource = ObjectWrapper.convert(toWrap);
     }
 
     public IBlock getBlock()
     {
-        return this.dispenserBlock.getWorld().getBlockAt(
-                this.dispenserBlock.getX(),
-                this.dispenserBlock.getY(),
-                this.dispenserBlock.getZ()
-        );
+        return blockProjectileSource.getBlock();
     }
 
     public <T extends RunsafeProjectile> T launchProjectile(Class<? extends T> projectile)
