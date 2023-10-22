4c95438b67b11bd220428437e6742e01b1fda00b
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-10-22 19:43:11 -0400

    New runsafe area effects
    For shenanigans involving dergons

---
+++ src/no/runsafe/framework/minecraft/entity/RunsafeAreaEffectCloud.java
@@ -0,0 +1,16 @@
+package no.runsafe.framework.minecraft.entity;
+
+import no.runsafe.framework.api.entity.IAreaEffectCloud;
+import no.runsafe.framework.internal.wrapper.entity.BukkitAreaEffectCloud;
+import org.bukkit.entity.AreaEffectCloud;
+
+public class RunsafeAreaEffectCloud extends BukkitAreaEffectCloud implements IAreaEffectCloud
+{
+    public RunsafeAreaEffectCloud(AreaEffectCloud toWrap)
+    {
+        super(toWrap);
+        cloud = toWrap;
+    }
+
+    private final AreaEffectCloud cloud;
+}
