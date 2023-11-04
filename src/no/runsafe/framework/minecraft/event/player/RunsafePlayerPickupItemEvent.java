81bc70909228eb5d0f3daf62e0b7fb2fc05ff4ec
Morten Nilsen <morten@runsafe.no>
2023-11-04 18:58:02 +0100

    Change base class

--- src/no/runsafe/framework/minecraft/event/player/RunsafePlayerPickupItemEvent.java
+++ src/no/runsafe/framework/minecraft/event/player/RunsafePlayerPickupItemEvent.java
@@ -1,11 +1,12 @@
 package no.runsafe.framework.minecraft.event.player;
 
+import no.runsafe.framework.api.event.CancellableEvent;
 import no.runsafe.framework.internal.wrapper.ObjectWrapper;
 import no.runsafe.framework.minecraft.entity.RunsafeItem;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
-public class RunsafePlayerPickupItemEvent extends RunsafePlayerEvent implements Cancellable
+public class RunsafePlayerPickupItemEvent extends RunsafeCancellablePlayerEvent
 {
 	public RunsafePlayerPickupItemEvent(PlayerPickupItemEvent toWrap)
 	{
@@ -21,18 +22,6 @@ public class RunsafePlayerPickupItemEvent extends RunsafePlayerEvent implements 
 	public int getRemaining()
 	{
 		return event.getRemaining();
-	}
-
-	@Override
-	public boolean isCancelled()
-	{
-		return event.isCancelled();
-	}
-
-	@Override
-	public void setCancelled(boolean cancel)
-	{
-		event.setCancelled(cancel);
 	}
 
 	private final PlayerPickupItemEvent event;
