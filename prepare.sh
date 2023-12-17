30e35ad18afc6eea2fcc42318317e5703f74ce13
Morten Nilsen <morten@runsafe.no>
2023-12-17 23:30:11 +0100

    conflict

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd ..
-mkdir spigotmc
-cd spigotmc
+mkdir target
+cd target
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar -o BuildTools.jar
 java -jar BuildTools.jar --rev 1.12.2
